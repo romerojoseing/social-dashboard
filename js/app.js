209e3ce07c294813702ab77f23b9a8a4a6741793
romerojoseing <romerojoseing@gmail.com>
2020-08-04 11:58:51 -0400

    dark

--- js/app.js
+++ js/app.js
@@ -1,13 +1,37 @@
-const checkbox = document.querySelector('#checkbox')
+let darkMode = localStorage.getItem('darkMode');
 
-checkbox.addEventListener('change', function(event) {
-  if(this.checked){
-    document.body.classList.remove('is-light-mode')
-    document.body.classList.add('is-dark-mode')
+const darkModeToggle = document.querySelector('#checkbox');
+
+const enableDarkMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('is-dark-mode');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('is-dark-mode');
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('darkMode', null);
+}
+
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  darkMode = localStorage.getItem('darkMode');
+
+  // if it not current enabled, enable it
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+    // if it has been enabled, turn it off  
+  } else {
+    disableDarkMode();
   }
-  else{
-    document.body.classList.remove('is-dark-mode')
-    document.body.classList.add('is-light-mode')
-  }
-  
-})
+});
